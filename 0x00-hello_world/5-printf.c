1c73390bafb84c9d097559c13c61ccdb6b56dfd4
YonasAbera <yonasabera007@gmail.com>
2022-12-08 15:50:05 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>                                                                                                                          
+#include <stdio.h>
+
 /**
- * main - main entry point
+ * main - main function
  *
- * Return: Always 0 (success)
+ * Return: 0 on (success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
